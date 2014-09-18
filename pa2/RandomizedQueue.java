e076563dae2ca8c69984a63c9c68738f21f858c7
MarkHampson <mark.hampson@gmail.com>
2014-09-17 21:40:38 -0600

    mostly finished RandomizedQueue

--- pa2/RandomizedQueue.java
+++ pa2/RandomizedQueue.java
@@ -1,8 +1,8 @@
 //import java.util.UnsupportedOperationException;
-//import java.util.NoSuchElementException;
+import java.util.NoSuchElementException;
 import java.util.Iterator;
-
-public class RandomizedQueue<Item> { //implements Iterable<Item>
+ 
+public class RandomizedQueue<Item> implements Iterable<Item>{ 
 
     private Item[] items;   //The generic array
     private int N;          //The item count
@@ -36,26 +36,85 @@ public class RandomizedQueue<Item> { //implements Iterable<Item>
     }
     
 //This cannot be in the final version (extra public method):
-    public String toString(){
-        String s = new String();
-        for(int i=0; i<size(); i++) s+= items[i];
-        return s;
+//    public String toString(){
+//      String s = new String();
+//      for(int i=0; i<size(); i++) s+= items[i];
+//      return s;
+//  }
+
+    //randomly select and remove an item
+    public Item dequeue(){
+        int randomIndex;
+        randomIndex = StdRandom.uniform(N);        
+        //swap the randomly selected index with the last element in the array
+        Item temp = items[N-1];
+        items[N-1] = items[randomIndex];
+        items[randomIndex] = temp; 
+        N--;
+        return items[N]; //N is now the last element since it was just decremented
+    }
+    //randomly select and return, but don't remove item
+    public Item sample(){
+        int randomIndex;
+        randomIndex = StdRandom.uniform(N);
+        return items[randomIndex];
+    }
+    //implement the iterator() method:
+    public Iterator<Item> iterator(){
+        return new RandomIterator<Item>(this);
+    }
+    //implement the Iterator<Item> class:
+    private class RandomIterator<Item> implements Iterator<Item>{
+        private RandomizedQueue<Item> iterateMe;
+        private int[] indexArray;
+        private int current;
+
+        public RandomIterator(RandomizedQueue<Item> iterateMe){
+            this.iterateMe = iterateMe;  //save the pointer    
+            indexArray = new int[iterateMe.size()];
+            for(int i=0; i<iterateMe.size(); i++){
+                indexArray[i] = i; //initialize by counting
+            }
+            //now randomize the indexing array
+            StdRandom.shuffle(indexArray);                
+            current = 0;
+        }
+        public boolean hasNext(){ 
+            return current < iterateMe.size();
+        }
+        public void remove(){ 
+            throw new UnsupportedOperationException(); 
+        }
+        public Item next(){
+            if(!hasNext()) throw new NoSuchElementException();
+            int index = indexArray[current]; //get the random index
+            Item item = iterateMe.items[index]; //retrieve the item
+            current++;
+            return item;
+        }
     }
 
 
-    //public Item dequeue()
-    //public Item sample()
-    //public Iterator<Item> iterator()
     public static void main(String[] args){
+        if(args.length < 2) throw new java.lang.IllegalArgumentException("More than one arg pls");        
         RandomizedQueue<String> myQ = new RandomizedQueue();
         System.out.println("Size: " + myQ.size());
         System.out.println("Empty?: " + myQ.isEmpty());
-        
         for(int i=0; i<args.length; i++){
             myQ.enqueue(args[i]);
+            System.out.print(args[i]);
         }
+        System.out.println("");
         System.out.println("Size: " + myQ.size());
         System.out.println("Empty?: " + myQ.isEmpty());
-        System.out.println(myQ);
+        //Give me an iterator
+        Iterator<String> iter1 = myQ.iterator();
+        //Give me another
+        Iterator<String> iter2 = myQ.iterator();
+        while( iter1.hasNext() || iter2.hasNext() ){
+            System.out.print(iter1.next());
+            System.out.print(iter2.next());
+            System.out.println("");
+        }
     }
 }
