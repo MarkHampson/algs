ba632489127b9044e55fcc6231aafb7a289d030e
MarkHampson <mark.hampson@gmail.com>
2014-11-10 22:28:52 -0700

    Solver actually solves now

--- pa4/Solver.java
+++ pa4/Solver.java
@@ -2,68 +2,79 @@ import java.util.Comparator;
 
 public class Solver {
     private int totalMoves = 0;
-    private Board prevBoard = null;
-    private Board prevBoardAlt = null;
 
     private class SearchNode {
         private Board board;
         private int numMoves;
         private int priority;
+        private int manPriority;
+        private SearchNode prevNode;
 
-        public SearchNode(Board b, int moves) {
+        public SearchNode(Board b, int moves, SearchNode prev) {
             board = b;
             numMoves = moves;
             priority = b.hamming() + moves;
+            manPriority = b.manhattan() + moves;
+            prevNode = prev;
+        }
+
+        private Iterable<Board> getNeighbors() {
+            return board.neighbors();
+        }
+
+        private boolean isLastBoard(Board b) {
+            if(prevNode == null) return false;
+            else return b.equals(prevNode.board);
+        }
+
+        private boolean isGoal() {
+            return board.isGoal();
         }
 
         public String toString() {
-            return board.toString() + "Priority = " + priority;
+            return board.toString() + "Ham Priority = " + priority + " Man Priority = " + manPriority;
         }
 
     }
 
     public Solver(Board initial) { //find a solution to the intial board
-        MinPQ<SearchNode> search = new MinPQ(HAMMING);   //search queue
-        MinPQ<SearchNode> altSearch = new MinPQ(HAMMING); //alternate search queue using twin
-        SearchNode init = new SearchNode(initial, totalMoves);
-        SearchNode initAlt = new SearchNode(initial.twin(), totalMoves);
+        MinPQ<SearchNode> search = new MinPQ(MANHATTAN);   //search queue
+        SearchNode init = new SearchNode(initial, totalMoves, null);
         search.insert(init);
-        altSearch.insert(initAlt);
-//        for(Board b: search) System.out.println(b);
-//        for(Board b: altSearch) System.out.println(b);
-//        for(Board b: search.min().board.neighbors()) System.out.println(b);
-        int count = 0;
-        while( (count < 1)) {
+           
+        while(true) { 
             SearchNode minPriority = search.delMin();
-            //System.out.println(minPriority.board);
-            //System.out.println("Priority = " + minPriority.priority);
+            System.out.println("Pop: ");
             System.out.println(minPriority);
-            for(Board b: minPriority.board.neighbors()) {
-                   if(!b.equals(prevBoard)) search.insert(new SearchNode(b, totalMoves)); 
-            }
-            for(SearchNode s: search) System.out.println(s);
-
-            prevBoard = minPriority.board;
-//            SearchNode minPriorityAlt = altSearch.delMin();
-//            for(Board b: minPriorityAlt.board.neighbors()) {
-//                   if(!b.equals(prevBoardAlt)) altSearch.insert(new SearchNode(b, totalMoves));
-//            }
-//            prevBoardAlt = minPriorityAlt.board;
+            System.out.println("Min Priority numMoves: " + minPriority.numMoves);
             totalMoves++;
-            count++;
-        }
+            for(Board b: minPriority.getNeighbors()) {
+                   if(!minPriority.isLastBoard(b)) search.insert(new SearchNode(b, totalMoves, minPriority)); 
+            }
 
-        //System.out.println(search.min().board);
-        //System.out.println("Priority = " + search.min().priority);
-        //System.out.println(altSearch.min().board);
+            if(minPriority.isGoal()) {
+                System.out.println("Goal! " + minPriority);
+                break;
+            }
+        }
     } 
 
-    public final Comparator<SearchNode> HAMMING = new HamComparator();
+    private final Comparator<SearchNode> HAMMING = new HamComparator();
 
     private class HamComparator implements Comparator<SearchNode> {
         public int compare(SearchNode s1, SearchNode s2) {
             if(s1.priority < s2.priority) return -1;
             if(s1.priority > s2.priority) return 1;
+            return 0;
+        }
+    }
+
+    private final Comparator<SearchNode> MANHATTAN = new ManComparator();
+
+    private class ManComparator implements Comparator<SearchNode> {
+        public int compare(SearchNode s1, SearchNode s2) {
+            if(s1.manPriority < s2.manPriority) return -1;
+            if(s1.manPriority > s2.manPriority) return 1;
             return 0;
         }
     }
@@ -74,13 +85,14 @@ public class Solver {
     //public Iterable<Board> solution()   // sequence of boards in a shortest solution
 
     public static void main(String[] args) {  // solve a slider puzzle
-        int[][] test = new int[][] {
-            new int[] {1, 2, 5},
-            new int[] {4, 0, 6},
-            new int[] {7, 8, 3}
-        };
-        Board board = new Board(test);
-        Solver solve = new Solver(board);
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
     }
 
 }
